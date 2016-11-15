142132f66a57af09164794af7701cc0d8bbc9a2a
Maxime <ribera.maxime@gmail.com>
2016-11-15 14:24:28 +0100

    Test notifications

--- www/js/app.js
+++ www/js/app.js
@@ -5,22 +5,21 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('wakeupApp', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('wakeupApp', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
-     $ionicPlatform.ready(function() {
-                          // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                          // for form inputs)
-                          if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-                          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                          cordova.plugins.Keyboard.disableScroll(true);
-                          
-                          }
-                          if (window.StatusBar) {
-                          StatusBar.styleDefault();
-                          }
-                          });
-     })
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
 
 .config(function($stateProvider, $urlRouterProvider) {
         
--- www/js/app/alarm/alarms.controller.js
+++ www/js/app/alarm/alarms.controller.js
@@ -1,5 +1,5 @@
 angular.module('wakeupApp')
-  .controller('AlarmsCtrl', function ($scope, $rootScope) {
+  .controller('AlarmsCtrl', function ($scope, $rootScope, $ionicPopup) {
       $scope.alarms = [
         {
           id: 1,
@@ -12,4 +12,48 @@ angular.module('wakeupApp')
           date: new Date()
         }
       ];
+
+    var successCallback = function(result) {
+      var message;
+      console.log
+      if (result.type==='wakeup') {
+        message = 'wakeup alarm detected--' + result.extra;
+      } else if(result.type==='set'){
+        message = 'wakeup alarm set--' + result;
+      } else {
+        message = 'wakeup unhandled type (' + result.type + ')';
+      }
+
+        $ionicPopup.alert({
+          title: 'Success !',
+          template: message
+        }).then(function(res) {
+        });
+    };
+
+    var errorCallback = function(result) {
+      $ionicPopup.alert({
+        title: 'Error !',
+        template: 'Error while creating alarm'
+      }).then(function(res) {
+      });
+    };
+
+    //console.log(cordova);
+    //console.log(window);
+    //console.log(navigator.plugins);
+
+    window.wakeuptimer.wakeup(
+      successCallback,
+      errorCallback,
+      // a list of alarms to set
+      {
+        alarms : [{
+          type : 'onetime',
+          time : { hour : 11, minute : 20 },
+          extra : { message : 'json containing app-specific information to be posted when alarm triggers' },
+          message : 'Alarm has expired!'
+        }]
+      }
+    );
   })
