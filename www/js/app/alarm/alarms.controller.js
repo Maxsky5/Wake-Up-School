e7f1aca6b18e4e3263c90bb0d7abbe8494361cf3
Maxime <ribera.maxime@gmail.com>
2016-11-16 15:24:59 +0100

    Ajout des paramètres et du LocalStorage

--- www/js/app/alarm/alarms.controller.js
+++ www/js/app/alarm/alarms.controller.js
@@ -32,4 +32,4 @@ angular.module('wakeupApp')
       //});
 
     }
-  })
+  });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,10 +17,4 @@ angular.module('starter.controllers', [])
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
 });
