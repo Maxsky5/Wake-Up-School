daaac31523fc31e6c6f49a05cfb0069a1d3f47e1
Maxime <ribera.maxime@gmail.com>
2016-11-16 10:37:30 +0100

    Facking Access-Control-Allow-Origin

--- www/js/app.js
+++ www/js/app.js
@@ -8,7 +8,7 @@
 angular.module('wakeupApp', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform, $http) {
-  $ionicPlatform.ready(function() {
+  $ionicPlatform.ready(function($http) {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
@@ -18,89 +18,90 @@ angular.module('wakeupApp', ['ionic', 'ngCordova', 'starter.controllers', 'start
     if (window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+    //amMoment.changeLocale('fr');
   });
 
   $http.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 })
 
-.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+  .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
 
-        $httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+      // Ionic uses AngularUI Router which uses the concept of states
+      // Learn more here: https://github.com/angular-ui/ui-router
+      // Set up the various states which the app can be in.
+      // Each state's controller can be found in controllers.js
+      $stateProvider
 
-        // Ionic uses AngularUI Router which uses the concept of states
-        // Learn more here: https://github.com/angular-ui/ui-router
-        // Set up the various states which the app can be in.
-        // Each state's controller can be found in controllers.js
-        $stateProvider
-        
-        // setup an abstract state for the tabs directive
-        .state('tab', {
-               url: '/tab',
-               abstract: true,
-               templateUrl: 'templates/tabs.html'
-               })
-        
-        // Each tab has its own nav history stack:
-        
-        .state('tab.dash', {
-               url: '/dash',
-               views: {
-               'tab-dash': {
-               templateUrl: 'templates/tab-dash.html',
-               controller: 'DashCtrl'
-               }
-               }
-               })
-        
-        .state('tab.chats', {
-               url: '/chats',
-               views: {
-               'tab-chats': {
-               templateUrl: 'templates/tab-chats.html',
-               controller: 'ChatsCtrl'
-               }
-               }
-               })
-        .state('tab.chat-detail', {
-               url: '/chats/:chatId',
-               views: {
-               'tab-chats': {
-               templateUrl: 'templates/chat-detail.html',
-               controller: 'ChatDetailCtrl'
-               }
-               }
-               })
-        .state('tab.alarms', {
-               url: '/alarms',
-               views: {
-               'tab-alarms': {
-               templateUrl: 'templates/tab-alarms.html',
-               controller: 'AlarmsCtrl'
-               }
-               }
-               })
-        .state('tab.alarm-detail', {
-               url: '/alarms/:alarmId',
-               views: {
-               'tab-chats': {
-               templateUrl: 'templates/alarm-detail.html',
-               controller: 'AlarmDetailCtrl'
-               }
-               }
-               })
-        
-        .state('tab.account', {
-               url: '/account',
-               views: {
-               'tab-account': {
-               templateUrl: 'templates/tab-account.html',
-               controller: 'AccountCtrl'
-               }
-               }
-               });
-        
-        // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/tab/dash');
-        
-        });
+      // setup an abstract state for the tabs directive
+      .state('tab', {
+             url: '/tab',
+             abstract: true,
+             templateUrl: 'templates/tabs.html'
+             })
+
+      // Each tab has its own nav history stack:
+
+      .state('tab.dash', {
+             url: '/dash',
+             views: {
+             'tab-dash': {
+             templateUrl: 'templates/tab-dash.html',
+             controller: 'DashCtrl'
+             }
+             }
+             })
+
+      .state('tab.chats', {
+             url: '/chats',
+             views: {
+             'tab-chats': {
+             templateUrl: 'templates/tab-chats.html',
+             controller: 'ChatsCtrl'
+             }
+             }
+             })
+      .state('tab.chat-detail', {
+             url: '/chats/:chatId',
+             views: {
+             'tab-chats': {
+             templateUrl: 'templates/chat-detail.html',
+             controller: 'ChatDetailCtrl'
+             }
+             }
+             })
+
+      .state('tab.alarms', {
+             url: '/alarms',
+             views: {
+             'tab-alarms': {
+             templateUrl: 'templates/tab-alarms.html',
+             controller: 'AlarmsCtrl'
+             }
+             }
+             })
+      .state('tab.alarm-detail', {
+             url: '/alarms/:alarmId',
+             views: {
+             'tab-chats': {
+             templateUrl: 'templates/alarm-detail.html',
+             controller: 'AlarmDetailCtrl'
+             }
+             }
+             })
+
+      .state('tab.account', {
+             url: '/account',
+             views: {
+             'tab-account': {
+             templateUrl: 'templates/tab-account.html',
+             controller: 'AccountCtrl'
+             }
+             }
+             });
+
+      // if none of the above states are matched, use this as the fallback
+      $urlRouterProvider.otherwise('/tab/dash');
+
+  });
 
--- www/js/app/courses/courses.service.js
+++ www/js/app/courses/courses.service.js
@@ -20,9 +20,9 @@ angular.module('wakeupApp')
         return courses;
     }
 
-    return {        
+    return {
         get : function(date) {
-            u ='https://edtmobilite.wigorservices.net/WebPsDyn.aspx';
+            var u ='http://edtmobilite.wigorservices.net/WebPsDyn.aspx';
             u += '?Action=posETUD&serverid=f&tel=' + login + '&date=';
             u += $filter('date')(date, "MM/dd/yyyy'%20'HH:mm");
 
@@ -31,17 +31,18 @@ angular.module('wakeupApp')
                         // Process the HTML before passing data to
                         // controller
                         console.log("Réponse");
+                        console.log(response);
                         /*
                         var parser = new DOMParser();
                         var doc = parser.parseFromString(response.data,
                                                          'text/html');
-                        
+
                         var coursesRootElem = doc.getElementById('Region');
                         if (coursesRootElem == null)
                             // TODO: Handle error: Invalid html file
                             return [];
                         courses = getCoursesList(coursesRootElem);
-                      
+
                         console.log(courses);
 
                         // TODO:
